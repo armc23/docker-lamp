cbb772cc077f7694075e7ed6e341d0902051edbb
armc23 <mantashyan.armen@gmail.com>
2023-01-31 13:39:10 -0600

    api working state

--- data/inc/config.php
+++ data/inc/config.php
@@ -1,5 +1,5 @@
 <?php
-define("DB_HOST", "mysql");
+define("DB_HOST", "mysql-custom");
 define("DB_USERNAME", "dev");
 define("DB_PASSWORD", "!superSecretnax");
 define("DB_DATABASE_NAME", "api_dev");
