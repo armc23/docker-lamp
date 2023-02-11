50684e6779d20c3f2042535707836f7fbe3bc0bb
armc23 <mantashyan.armen@gmail.com>
2023-02-11 15:45:31 -0600

    Rewriting PHP code and ading POST request working state

--- data/inc/config.php
+++ data/inc/config.php
@@ -10,4 +10,12 @@ define('DB_USERNAME', $USERNAME );
 define('DB_PASSWORD', $DB_PASSWORD);
 define('DB_DATABASE_NAME', $DB_DATABASE_NAME);
 
+$conn = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE_NAME);
+
+if(!$conn){
+
+    die("Connection Failed: " . mysqli_connect_error());
+}
+
+
 ?>
